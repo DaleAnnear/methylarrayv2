9d9ee739b3986004023b0653071df422df9980fd
ajandria <adrian.janucik@gmail.com>
2025-03-02 19:26:50 +0100

    pre-commit fix remove_confounding_probes_o2.R

--- modules/local/remove_confounding_probes/templates/remove_confounding_probes_o2.R
+++ modules/local/remove_confounding_probes/templates/remove_confounding_probes_o2.R
@@ -2,17 +2,17 @@
 
 ###General script to correct for confounders: age and remove probes associated to age, BMI and
 ##input=Beta values.rdata
-##output=Beta values.rdata 
+##output=Beta values.rdata
 
 library(dplyr)
 library(readr)
 library(minfi)
 library(tibble)
-library(doParallel) 
+library(doParallel)
 
 registerDoParallel(makePSOCKcluster(3))
 
-##Constant: p-value threshold 
+##Constant: p-value threshold
 t = 0.05
 
 ##Load previously saved data (RData objects, for more details, please look at pre-processing.Rmd)
@@ -23,7 +23,8 @@ assays(mSetSqFlt, withDimnames = FALSE) [["M"]] <- mVals
 assays(mSetSqFlt, withDimnames = FALSE) [["Beta"]] <- bVals
 
 metadata <- read_csv("$extensive_metadata") %>%
-            filter(sample_id %in% c(colnames(bVals))) 
+    filter(sample_id %in% c(colnames(bVals)))
+
 ####Match metadata order to columns in bVals
 metadata <- metadata[match(c(colnames(mVals)), metadata\$sample_id),]
 
@@ -33,16 +34,15 @@ CONFOUNDER <- metadata\$age
 ###Removing probes with high correlation to a confounder
 dmp.conf.sig <- dmpFinder(as.matrix(mVals), CONFOUNDER, type = "categorical",
                 shrinkVar = TRUE # For development as otherwise crashes
-                ) %>% 
+                ) %>%
                 rownames_to_column("probe") %>%
                 filter(qval < t) %>%
-                dplyr::select(probe) 
+                dplyr::select(probe)
 
 ###Remove probes related to sex, age and bmi
-dmp.conf.sig <- as_tibble(rownames(mVals)) %>% 
-  anti_join(dmp.conf.sig %>% 
-              dplyr::select(probe),
-            by = c("value" = "probe"))
+dmp.conf.sig <- as_tibble(rownames(mVals)) %>%
+    anti_join(dmp.conf.sig %>%
+    dplyr::select(probe), by = c("value" = "probe"))
 mVals <- mVals[dmp.conf.sig\$value,]
 bVals <- bVals[dmp.conf.sig\$value,]
 mSetSqFlt <- mSetSqFlt[dmp.conf.sig\$value,]
