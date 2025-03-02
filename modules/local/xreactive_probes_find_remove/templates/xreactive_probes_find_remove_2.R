06f05347dbc0cb1289d33ede6ec3fe97b5d8fbd9
ajandria <adrian.janucik@gmail.com>
2025-03-02 19:33:57 +0100

    pre-commit fix xreactive_probes_find_remove_2.R

--- modules/local/xreactive_probes_find_remove/templates/xreactive_probes_find_remove_2.R
+++ modules/local/xreactive_probes_find_remove/templates/xreactive_probes_find_remove_2.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 ### This script finds cross reactive probes in IlluminaEPIC array and removes them, It is also valid for all types of Illumina arrays
-### input = normalized  and filtered mSet object 
+### input = normalized  and filtered mSet object
 ##output = mSet object (.RData format), normalized  M and beta values (csv format)
 
 library(minfi)
@@ -14,9 +14,9 @@ library(dplyr)
 library(readr)
 
 # If using conda try to fetch hosted .tar.gz DNAmCrosshyb package
-if("$conda") {
-  message("Fetching DNAmCrosshyb tarball from https://github.com/ajandria/DNAmCrosshyb/releases/download/0.0.0.9000-methylarray-host/DNAmCrosshyb_0.0.0.9000.tar.gz")
-  install.packages("https://github.com/ajandria/DNAmCrosshyb/releases/download/0.0.0.9000-methylarray-host/DNAmCrosshyb_0.0.0.9000.tar.gz", repos = NULL, type = "source")
+if ("$conda") {
+    message("Fetching DNAmCrosshyb tarball from https://github.com/ajandria/DNAmCrosshyb/releases/download/0.0.0.9000-methylarray-host/DNAmCrosshyb_0.0.0.9000.tar.gz")
+    install.packages("https://github.com/ajandria/DNAmCrosshyb/releases/download/0.0.0.9000-methylarray-host/DNAmCrosshyb_0.0.0.9000.tar.gz", repos = NULL, type = "source")
 }
 library(DNAmCrosshyb)
 
@@ -25,10 +25,10 @@ RData_PREPROCESSING <- "$RData_PREPROCESSING"
 Genome_PATH <- "$genome_path"
 
 # If input is a compressed archive, extract it
-if(grepl(".tar.gz", Genome_PATH)) {
-  message("Extracting ", Genome_PATH)
-  system(paste("tar -zxvf", Genome_PATH))
-  Genome_PATH <- gsub(".tar.gz\$", "", Genome_PATH)
+if (grepl(".tar.gz", Genome_PATH)) {
+    message("Extracting ", Genome_PATH)
+    system(paste("tar -zxvf", Genome_PATH))
+    Genome_PATH <- gsub(".tar.gz\$", "", Genome_PATH)
 }
 
 ####The human genome version should be downloaded to data/genome_bs and here we specify the file 
@@ -43,34 +43,35 @@ STEP = 5
 ##Read prestored data (see pre-processing.Rmd for more details)
 get(load(RData_PREPROCESSING))
 
-##Cross-reactive probes detection, 
+##Cross-reactive probes detection,
 probes <- rownames(mSetSqFlt)
-matches <- map_probes(probes, path = PATH, 
-                      # chromosomes was 'all' but is reduced to 3 for testing
-                      array = "EPIC", chromosomes = "$chrom_number", min_width = MIN, max_width = MAX, step_size = STEP, 
-                      allow_mismatch = FALSE, allow_INDEL = FALSE, verbose = TRUE)
+matches <- map_probes(
+    probes, path = PATH,
+    # chromosomes was 'all' but is reduced to 3 for testing
+    array = "EPIC", chromosomes = "$chrom_number", min_width = MIN, max_width = MAX, step_size = STEP,
+    allow_mismatch = FALSE, allow_INDEL = FALSE, verbose = TRUE
+)
 
 ###Identify probes mapping to multiple sites on the genome
 nr_matches <- get_nr_matches_per_probe(matches)
-nr_matches %>% 
-  data.frame() %>% 
-  ###Choose minimum number of bp for the mismatch to be retained: 30, 35, 40, 45 or 50
-  filter(bp30 != "1" | bp35 != "1" | bp40 != "1" #| bp45 != "1" | bp50 != "1" # commented out due to: "chromosomes was 'all' but is reduced to 2 for testing"
-  ) %>% 
-  select(Probe) %>%
-  write_csv("x_reactive_probes.csv")
+nr_matches %>%
+    data.frame() %>%
+    ###Choose minimum number of bp for the mismatch to be retained: 30, 35, 40, 45 or 50
+    filter(bp30 != "1" | bp35 != "1" | bp40 != "1") %>% #| bp45 != "1" | bp50 != "1" # commented out due to: "chromosomes was 'all' but is reduced to 2 for testing"
+    select(Probe) %>%
+    write_csv("x_reactive_probes.csv")
 
-rep_matches <- find_repeat_overlaps(matches, genome_build = "hg19", min_overlap = "any") 
-  rep_matches %>%      ####manually check the number of probes to decide he width threshold you want to have
-    filter(repeat_overlap == "TRUE") %>% 
-    filter(width == MAX) %>% 
+rep_matches <- find_repeat_overlaps(matches, genome_build = "hg19", min_overlap = "any")
+rep_matches %>% # manually check the number of probes to decide he width threshold you want to have
+    filter(repeat_overlap == "TRUE") %>%
+    filter(width == MAX) %>%
     distinct(Probe)
-  
+
 nr_matches <- as.data.frame(nr_matches)
-  
+
 ##Remove cross-reactive probes from list I made
 keep <- !(featureNames(mSetSqFlt) %in% nr_matches\$Probe)
-mSetSqFlt <- mSetSqFlt[keep,] 
+mSetSqFlt <- mSetSqFlt[keep,]
 
 ##Calculate M and Beta values
 mVals <- getM(mSetSqFlt)
@@ -80,5 +81,3 @@ bVals <- getBeta(mSetSqFlt)
 write_csv(as.data.frame(mVals), "mVals_noXprob.csv")
 write_csv(as.data.frame(bVals), "bVals_noXprob.csv")
 save(mSetSqFlt, file = "mSetSqFlt_noXprob.RData")
-
-
